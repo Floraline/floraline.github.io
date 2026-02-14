fb09bdaf4fdaf0a467edb746061dc7bf0ac705e8
anne <138407843+Floraline@users.noreply.github.com>
2026-02-14 00:15:45 -0400

    Implement password verification and create element on click
    Add password verification and dynamic element creation.

---
+++ script.js
@@ -0,0 +1,20 @@
+
+ var  B = function verifPassword() {
+    var A = document.querySelector("#password").value ;
+    var D = document.querySelector("#name").value ;
+     if (A == "Caca") {
+         window.location.href = "page 2.html";
+     } else {
+         alert("Sorry " + D + ", le mot de Passe est inccoret.");
+     }
+} ;
+document.querySelector("#loginBtn").addEventListener("click", B) ;
+
+var lol = function indice (){
+    var p = document.createElement("div");
+    var p2 = document.createTextNode("p")= "lol";
+    p.appendChild(p2);
+};
+button = document.querySelector("#indice").addEventListener("click", lol);
+
+
